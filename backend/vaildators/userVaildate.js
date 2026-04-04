aced214c45f04f9e03cbac7b94fa1a24fa77ff6a
nichaphat <nichaphat.siribut22@gmail.com>
2026-04-04 16:00:59 +0700

    feat: change-password

---
+++ backend/vaildators/userVaildate.js
@@ -0,0 +1,23 @@
+import yup from "yup";
+
+export const userSchema = yup.object({
+  username: yup
+    .string()
+    .trim()
+    .min(3, "Username must be at least of 3 characters")
+    .required(),
+  email: yup.string().email("The email is not valid one").required(),
+  password: yup
+    .string()
+    .min(4, "Password must be at least 4 character")
+    .required(),
+});
+
+export const validateUser = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: error.errors });
+  }
+};
